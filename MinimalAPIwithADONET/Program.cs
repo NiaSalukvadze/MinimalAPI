9abb2bc8a7a9411cb1b546523290691e7d1afe37
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-05-08 14:16:05 +0400

    Add files via upload

---
+++ MinimalAPIwithADONET/Program.cs
@@ -0,0 +1,39 @@
+namespace MinimalAPIwithADONET;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddMvc();
+
+        var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+        builder.Services.AddSingleton(configuration.GetConnectionString("DefaultConnection"));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+        app.Run();
+    }
+}

ceabdd221fea282c4359891004cd7e6920227d12
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-05-14 14:53:34 +0400

    Add files via upload

---
+++ MinimalAPIwithCQRS_EF_Dapper/CQRS/Commands/CreateProductCommand/CreateProductCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using MinimalAPIwithCQRS_EF_Dapper.Models;
+
+namespace MinimalAPIwithCQRS_EF_Dapper.CQRS.Commands.CreateProductCommand
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand>
+    {
+        private SalesContext _salesContext;
+        public CreateProductCommandHandler(SalesContext salesContext)
+        {
+            _salesContext = salesContext;
+        }
+        public async Task Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = request.Product;
+
+            _salesContext.Products.Add(product);
+
+            await _salesContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ MinimalAPIwithCQRS_EF_Dapper/CQRS/Commands/UpdateProductCommand/UpdateProductCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using MinimalAPIwithCQRS_EF_Dapper.Models;
+
+namespace MinimalAPIwithCQRS_EF_Dapper.CQRS.Commands.UpdateProductCommand
+{
+    public record UpdateProductCommand(int productId, Product product) : IRequest;
+}

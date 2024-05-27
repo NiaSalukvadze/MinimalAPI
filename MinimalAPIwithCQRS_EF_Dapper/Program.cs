501da25f32635c88a08b67cbe16965f4a532a55c
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-05-27 15:39:59 +0400

    Add files via upload

---
+++ MinimalAPIwithCQRS_EF_Dapper/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MinimalAPIwithCQRS_EF_Dapper.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace MinimalAPIwithCQRS_EF_Dapper;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddMvc();
+
+        var programAssembly = typeof(Program).Assembly;
+
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(programAssembly));
+
+        var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+        builder.Services.AddDbContext<SalesContext>(
+                options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+
+        builder.Services.AddScoped<IDbConnection>(provider =>
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            return new SqlConnection(connectionString);
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+        app.Run();
+    }
+}

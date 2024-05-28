a819b2da190b862d2639956a3ff0aa6c278e49d9
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-05-28 17:07:58 +0400

    Add files via upload

---
+++ MinimalAPIwithCQRS_EF_Dapper/CQRS/Queries/GetProducts/GetProductsQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using MinimalAPIwithCQRS_EF_Dapper.Models;
+
+namespace MinimalAPIwithCQRS_EF_Dapper.CQRS.Queries.GetProducts
+{
+    public sealed record GetProductsQuery() : IRequest<List<Product>>;
+}
---
+++ MinimalAPIwithCQRS_EF_Dapper/Models/Shipper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinimalAPIwithCQRS_EF_Dapper.Models;
+
+public partial class Shipper
+{
+    public int ShipperId { get; set; }
+
+    public string CompanyName { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}

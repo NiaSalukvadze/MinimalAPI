5e255dbcc0c59408a86fbf705e29d34dd3e3e3fc
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-05-28 17:05:36 +0400

    Add files via upload

---
+++ MinimalAPIwithCQRS_Dapper_EF/CQRS/Commands/CreateProductCommand/CreateProductCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using MediatR;
+using MinimalAPIwithCQRS_Dapper_EF.Models;
+using System.Data;
+
+namespace MinimalAPIwithCQRS_Dapper_EF.CQRS.Commands.CreateProductCommand
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand>
+    {
+        private readonly SalesContext _context;
+        public CreateProductCommandHandler(SalesContext context)
+        {
+            _context = context;
+        }
+        public async Task Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = request.Product;
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}

6691003d08a5553aa90782f90501f4f91695e975
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-05-29 15:41:52 +0400

    Add files via upload

---
+++ MinimalAPIwithCQRS_EF_Dapper/CQRS/Commands/UpdateProductCommand/UpdateProductCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using MediatR;
+using System.Data;
+
+namespace MinimalAPIwithCQRS_EF_Dapper.CQRS.Commands.UpdateProductCommand
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, Unit>
+    {
+        private readonly IDbConnection _dbConnection;
+        public UpdateProductCommandHandler(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+        public async Task<Unit> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = request.Product;
+
+            const string sql = @"update Products 
+                                 set ProductName = @productName,
+                                 SupplierId = @supplierId, 
+                                 CategoryId = @categoryId
+                                 where ProductId = @productId";
+
+            await _dbConnection.ExecuteAsync(sql, new
+            {
+                productId = product.ProductId,
+                productName = product.ProductName,
+                supplierId = product.SupplierId,
+                categoryId = product.CategoryId
+            });
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ MinimalAPIwithCQRS_EF_Dapper/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MinimalAPIwithCQRS_EF_Dapper.CQRS.Commands.CreateProductCommand;
+using MinimalAPIwithCQRS_EF_Dapper.CQRS.Commands.DeleteProductCommand;
+using MinimalAPIwithCQRS_EF_Dapper.CQRS.Commands.UpdateProductCommand;
+using MinimalAPIwithCQRS_EF_Dapper.CQRS.Queries.GetProducts;
+
+namespace MinimalAPIwithCQRS_EF_Dapper
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public sealed class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetProducts(CancellationToken cancellationToken)
+        {
+            var query = new GetProductsQuery();
+
+            var products = await _mediator.Send(query, cancellationToken);
+            
+            return Ok(products);
+        }
+        
+        [HttpPost("create")]
+        public async Task<ActionResult> CreateProduct([FromBody] CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(command, cancellationToken);
+
+            return Ok();
+        }
+
+        [HttpPost("update")]
+        public async Task<ActionResult> UpdateProduct([FromBody] UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(command, cancellationToken);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct([FromBody] DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(command, cancellationToken);
+
+            return Ok();
+        }
+
+        /*
+        
+  { "product": {
+    "productName": "test",
+    "supplierId": 1,
+    "categoryId": 1,
+    "quantityPerUnit": "string",
+    "unitPrice": 0,
+    "unitsInStock": 0,
+    "unitsOnOrder": 0,
+    "reorderLevel": 0,
+    "discontinued": true
+    }
+  }
+*/
+
+
+
+    }
+}

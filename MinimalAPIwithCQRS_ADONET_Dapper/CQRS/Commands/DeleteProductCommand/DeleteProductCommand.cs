5299103c4ff7615f4493a0a47fb0aeaab12748da
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-05-29 15:34:06 +0400

    Add files via upload

---
+++ MinimalAPIwithCQRS_ADONET_Dapper/CQRS/Commands/DeleteProductCommand/DeleteProductCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace MinimalAPIwithCQRS_ADONET_Dapper.CQRS.Commands.DeleteProductCommand
+{
+    public record DeleteProductCommand(int productId) : IRequest<Unit>;
+
+}
---
+++ MinimalAPIwithCQRS_ADONET_Dapper/CQRS/Commands/UpdateProductCommand/UpdateProductCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using MinimalAPIwithCQRS_ADONET_Dapper.Models;
+
+namespace MinimalAPIwithCQRS_ADONET_Dapper.CQRS.Commands.UpdateProductCommand
+{
+    public record UpdateProductCommand(Product Product) : IRequest<Unit>;
+}

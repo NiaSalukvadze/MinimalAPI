76cc68adcbfa7312610a4ca1baea676b3427a3fb
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-06-10 15:49:39 +0400

    Delete MinimalAPIwithCQRS_EF_Dapper directory

--- MinimalAPIwithCQRS_EF_Dapper/CQRS/Commands/DeleteProductCommand/DeleteProductCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using MediatR;
-
-namespace MinimalAPIwithCQRS_EF_Dapper.CQRS.Commands.DeleteProductCommand
-{
-    public record DeleteProductCommand(int productId) : IRequest<Unit>;
-
-}
--- MinimalAPIwithCQRS_EF_Dapper/CQRS/Commands/UpdateProductCommand/UpdateProductCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using MediatR;
-using MinimalAPIwithCQRS_EF_Dapper.Models;
-
-namespace MinimalAPIwithCQRS_EF_Dapper.CQRS.Commands.UpdateProductCommand
-{
-    public record UpdateProductCommand(Product Product) : IRequest<Unit>;
-}

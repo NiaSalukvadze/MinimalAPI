c6dbfa582fc7168651b10b2dc7bf35cd33841349
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-06-10 15:55:06 +0400

    Add files via upload

---
+++ MinimalAPIwithCQRS_EF_Dapper/CQRS/Commands/DeleteProductCommand/DeleteProductCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System.Data;
+
+namespace MinimalAPIwithCQRS_EF_Dapper.CQRS.Commands.DeleteProductCommand
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, Unit>
+    {
+        private readonly IDbConnection _dbConnection;
+        public DeleteProductCommandHandler(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+        public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            const string sql = "DELETE FROM Products WHERE ProductID = (SELECT TOP 1 ProductID FROM Products ORDER BY ProductID DESC)";
+
+            await _dbConnection.ExecuteAsync(sql);
+
+            return Unit.Value;
+        }
+    }
+}

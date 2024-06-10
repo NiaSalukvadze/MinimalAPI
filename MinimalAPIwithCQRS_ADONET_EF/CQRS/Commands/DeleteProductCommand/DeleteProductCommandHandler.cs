6200daf5a7c917bdf24f9bda9aa264b9cb308e98
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-06-10 15:52:16 +0400

    Add files via upload

---
+++ MinimalAPIwithCQRS_ADONET_EF/CQRS/Commands/DeleteProductCommand/DeleteProductCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MinimalAPIwithCQRS_ADONET_EF.Models;
+
+namespace MinimalAPIwithCQRS_ADONET_EF.CQRS.Commands.DeleteProductCommand
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand>
+    {
+        private readonly SalesContext _context;
+        public DeleteProductCommandHandler(SalesContext context)
+        {
+            _context = context;
+        }
+        public async Task Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.OrderByDescending(x => x.ProductId).FirstOrDefaultAsync();
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

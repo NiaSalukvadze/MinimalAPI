633b09ad7235e8e3ea629c66b0908cd9f0dbdea2
NiaSalukvadze <99618208+NiaSalukvadze@users.noreply.github.com>
2024-06-13 16:28:06 +0400

    Add files via upload

---
+++ MinimalAPIwithEFCore/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinimalAPIwithEFCore.Models;
+
+public partial class ProductsAboveAveragePrice
+{
+    public string ProductName { get; set; } = null!;
+
+    public decimal? UnitPrice { get; set; }
+}
---
+++ MinimalAPIwithEFCore/ProductsController.cs
@@ -0,0 +1,81 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinimalAPIwithEFCore.Models;
+using System;
+
+namespace MinimalAPIwithEFCore
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly SalesContext _context;
+
+        public ProductsController(SalesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.ProductId }, product);
+        }
+
+        [HttpPost("update")]
+        public async Task<ActionResult<Product>> UpdateProduct(Product prod)
+        {
+            var productId = prod.ProductId;
+
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.ProductId == productId);
+
+            product.ProductName = prod.ProductName;
+            product.SupplierId = prod.SupplierId;
+            product.CategoryId = prod.CategoryId;
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.ProductId }, product);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct()
+        {
+            var product = await _context.Products.OrderByDescending(x => x.ProductId).FirstOrDefaultAsync();
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
